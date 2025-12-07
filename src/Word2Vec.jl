7da518eda5d8e9e2bfdc2c8812305a3942d5039a
Maximilian Hans <hans.maximilian@icloud.com>
2025-12-07 19:09:26 +0100

    m

--- src/Word2Vec.jl
+++ src/Word2Vec.jl
@@ -1,5 +1,23 @@
 module Word2Vec
 
-# Write your package code here.
+export Word2VecModel,
+       load_word2vec_text,
+       load_word2vec_binary,
+       get_embedding,
+       train_skipgram,
+       train_cbow,
+       conec_embedding
+
+include("models/Word2VecModel.jl")
+
+include("loaders/text_loader.jl")
+include("loaders/binary_loader.jl")
+
+include("training/skipgram.jl")
+include("training/cbow.jl")
+
+include("conec/conec.jl")
+
+include("utils/math_utils.jl")
 
 end
