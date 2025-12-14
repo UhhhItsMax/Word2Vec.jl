3dae8c4f51ab52241b74e18267a9bb470dcc398a
Maximilian Hans <hans.maximilian@icloud.com>
2025-12-14 21:41:46 +0100

    CI: tests+docs deploy; Pages landing; versioned docs

--- docs/make.jl
+++ docs/make.jl
@@ -3,22 +3,29 @@ using Documenter
 
 DocMeta.setdocmeta!(Word2Vec, :DocTestSetup, :(using Word2Vec); recursive=true)
 
-makedocs(;
-    modules=[Word2Vec],
-    authors="Maximilian Hans hans.maximilian@icloud.com",
-    sitename="Word2Vec.jl",
-    format=Documenter.HTML(;
-        canonical="https://UhhhItsMax.github.io/Word2Vec.jl",
-        edit_link="main",
-        assets=String[],
+makedocs(
+    modules = [Word2Vec],
+    authors = "Maximilian Hans <hans.maximilian@icloud.com>",
+    sitename = "Word2Vec.jl",
+    format = Documenter.HTML(
+        canonical = "https://uhhhitsmax.github.io/Word2Vec.jl/",
+        edit_link = "main",
     ),
-    pages=[
+    pages = [
         "Home" => "index.md",
         "Getting Started" => "getting_started.md",
     ],
 )
 
-deploydocs(;
-    repo="github.com/UhhhItsMax/Word2Vec.jl",
-    devbranch="main",
+deploydocs(
+    repo = "github.com/UhhhItsMax/Word2Vec.jl.git",
+    devbranch = "main",
+
+    devurl = "docs/dev",
+    versions = [
+        "docs/stable" => "v^",
+        "docs/dev"    => "dev",
+    ],
+
+    forcepush = true,
 )
