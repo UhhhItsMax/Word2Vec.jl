f84b3b5c36c34672c04b5b18e1ac0a8999e3f6d8
Paul Mathias Nelde <paul@funkhaus.io>
2026-01-19 12:57:48 +0100

    remove: unused files

--- src/Word2Vec.jl
+++ src/Word2Vec.jl
@@ -23,9 +23,7 @@ include("loaders/load_word2vec.jl")
 include("savers/save_word2vec.jl")
 
 include("utils/circular_buffer.jl")
-include("utils/math_utils.jl")
 
-include("training/skipgram.jl")
 include("training/cbow.jl")
 
 include("conec/conec.jl")
